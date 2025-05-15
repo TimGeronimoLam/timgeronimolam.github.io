e586f8fca8fca2dd7ef36422ea1a40fd1d84c3dd
Tim Lâm <tim.lam@threedy.io>
2025-05-16 01:30:49 +0200

    Initial commit

---
+++ src/content.config.ts
@@ -0,0 +1,27 @@
+// Import the glob loader
+import { glob } from "astro/loaders";
+// Import utilities from `astro:content`
+import { z, defineCollection } from "astro:content";
+
+// Define a `loader` and `schema` for each collection
+const blogSchema = z.object({
+  title: z.string(),
+  pubDate: z.date(),
+  description: z.string(),
+  author: z.string(),
+  image: z.object({
+    url: z.string(),
+    alt: z.string()
+  }),
+});
+
+const blog = defineCollection({
+  loader: glob({ pattern: '**/[^_]*.md', base: "./src/blog" }),
+  schema: blogSchema
+});
+
+// Create a TypeScript type from the Zod schema
+export type Frontmatter = z.infer<typeof blogSchema>;
+
+// Export a single `collections` object to register your collection(s)
+export const collections = { blog };
